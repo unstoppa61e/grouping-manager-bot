621b8ba59e6e17c75f41504c41038ad598f560e3
monoue <like_a_newborn_baby@icloud.com>
2021-07-21 16:20:24 +0900

    add: matching without bot, handling zero or one case

--- cogs/change_bot_nickname.py
+++ cogs/change_bot_nickname.py
@@ -1,5 +1,4 @@
 from discord.ext import commands
-from utils.check_privilege import executed_by_privileged_member
 
 
 class ChNick(commands.Cog):
@@ -8,8 +7,6 @@ class ChNick(commands.Cog):
 
     @commands.command()
     async def chnick(self, ctx):
-        if not executed_by_privileged_member(ctx):
-            return
         await ctx.guild.me.edit(nick="マッチングアプリ")
 
 
--- cogs/match.py
+++ cogs/match.py
@@ -13,7 +13,6 @@ class Match(commands.Cog):
 
     def init_embed(self) -> discord.Embed():
         embed = discord.Embed()
-        embed.color = discord.Color.green()
         return embed
 
     def make_text(self, author_mention) -> str:
@@ -22,40 +21,6 @@ class Match(commands.Cog):
     
     def make_line(self, room_num, room_members):
         return f"{room_num}号室: {' / '.join(room_members)}\n"
-    
-    # async def send_invitation_two_basis(self, ctx, users):
-    #     USERS_NUM = len(users)
-    #     ROOMS_NUM = USERS_NUM // 2
-    #     capacity_per_room = [2] * ROOMS_NUM
-    #     if USERS_NUM % 2 == 1:
-    #         capacity_per_room[0] += 1
-    #     start_i = 0
-    #     for i, capacity in enumerate(capacity_per_room, start=1):
-    #         end_i = start_i + capacity
-    #         self.embed.description = self.make_line(i, users[start_i:end_i])
-    #         await ctx.send(embed=self.embed)
-    #         start_i += capacity
-
-    # async def send_invitation_three_basis(self, ctx, users):
-    #     USERS_NUM = len(users)
-    #     ROOMS_NUM = math.ceil(USERS_NUM / 3.0)
-    #     capacity_per_room = [2] * ROOMS_NUM
-    #     REMAINDER = USERS_NUM % 3
-    #     for i in range(REMAINDER):
-    #         capacity_per_room[i] += 1
-    #     start_i = 0
-    #     for i, capacity in enumerate(capacity_per_room, start=1):
-    #         end_i = start_i + capacity
-    #         self.embed.description = self.make_line(i, users[start_i:end_i])
-    #         await ctx.send(embed=self.embed)
-    #         start_i += capacity
-
-    
-    # async def send_invitation(self, ctx, users, capacity_basis):
-    #     if capacity == 2:
-    #         await self.send_invitation_two_basis(ctx, users)
-    #     else:
-    #         await self.send_invitation_three_basis(ctx, users)
     
     def make_capacity_per_room_two_basis(self, users_num, capacity_basis):
         ROOMS_NUM = users_num // 2
@@ -71,7 +36,6 @@ class Match(commands.Cog):
         for i in range(REMAINDER):
             capacity_per_room[i] += 1
         return capacity_per_room
-
 
     def make_capacity_per_room(self, users_num, capacity_basis):
         if capacity_basis == 2:
@@ -92,6 +56,7 @@ class Match(commands.Cog):
     async def match(self, ctx):
         await self.bot.wait_until_ready()
         self.embed.description = self.make_text(ctx.author.mention)
+        self.embed.color = discord.Color.green()
         channel = ctx.channel
         msg = await ctx.send(embed=self.embed)
         emojis = ['✋', '2️⃣', '3️⃣']
@@ -106,7 +71,9 @@ class Match(commands.Cog):
         try:
             reaction, user = await self.bot.wait_for('reaction_add', timeout=timeout, check=check)
         except asyncio.TimeoutError:
-            await channel.send('募集から２カ月が経過したため、告知を締め切りました。')
+            self.embed.description = '募集から２カ月が経過したため、告知を締め切りました。'
+            self.embed.color = discord.Color.red()
+            await channel.send(embed=self.embed)
         else:
             capacity_basis = 2 if str(reaction.emoji) == '2️⃣' else 3
             cached_msg = discord.utils.get(self.bot.cached_messages, id=msg.id)
@@ -116,8 +83,15 @@ class Match(commands.Cog):
                 if str(reaction.emoji) == '✋':
                     reaction_users = await reaction.users().flatten()
                     for user in reaction_users:
+                        if user.bot:
+                            continue
                         users.append(user.mention)
                     break
+            if len(users) < 2:
+                self.embed.color = discord.Color.red()
+                self.embed.description = 'マッチングに必要な人数が集まりませんでした。'
+                await ctx.send(embed=self.embed)
+                return
             random.shuffle(users)
             await self.send_invitation(ctx, users, capacity_basis)
                     
