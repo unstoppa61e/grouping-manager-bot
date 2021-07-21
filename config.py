380b14b3c9212fbb20f2bb0712ef9a0a50378db8
monoue <like_a_newborn_baby@icloud.com>
2021-07-21 16:29:49 +0900

    change: config.py

--- config.py
+++ config.py
@@ -1,16 +1,3 @@
 import os
-import my_config
 
-# CHANNEL_ID = int(os.environ['CHANNEL_ID'])
-# NOTIFICATION_CHANNEL_ID = int(os.environ['NOTIFICATION_CHANNEL_ID'])
-# MESSAGE_ID = int(os.environ['MESSAGE_ID'])
-# TOKEN = os.environ['TOKEN']
-# GUILD_ID = int(os.environ['GUILD_ID'])
-# MONOUE_ID = int(os.environ['MONOUE_ID'])
-
-# CHANNEL_ID = my_config.CHANNEL_ID
-# MESSAGE_ID = my_config.MESSAGE_ID
-TOKEN = my_config.TOKEN
-# GUILD_ID = my_config.GUILD_ID
-# MONOUE_ID = my_config.MONOUE_ID
-# NOTIFICATION_CHANNEL_ID = my_config.NOTIFICATION_CHANNEL_ID
+TOKEN = os.environ['TOKEN']
