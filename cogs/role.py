8b41ac21b4dbcdd3d6da58a27332c72d5a255b79
monoue <like_a_newborn_baby@icloud.com>
2021-07-22 15:08:35 +0900

    add: toggling role with reaction

--- cogs/role.py
+++ cogs/role.py
@@ -39,24 +39,39 @@ class Role(commands.Cog):
     def get_emoji(self):
         return '✅'
 
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        if not payload.emoji.name == self.get_emoji():
+            return
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g: g.id == guild_id, self.bot.guilds)
+        member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+        await member.add_roles(self.role)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        if not payload.emoji.name == self.get_emoji():
+            return
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g: g.id == guild_id, self.bot.guilds)
+        member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+        await member.remove_roles(self.role)
 
     @commands.command()
     async def role(self, ctx):
         await self.bot.wait_until_ready()
         role_name = self.make_role_name(ctx.guild.roles)
-        role = await ctx.guild.create_role(name=role_name)
-        self.embed.description = self.make_text(role.mention, ctx.author.mention)
+        self.role = await ctx.guild.create_role(name=role_name)
+        self.embed.description = self.make_text(self.role.mention, ctx.author.mention)
         self.embed.color = discord.Color.green()
         channel = ctx.channel
         msg = await ctx.send(embed=self.embed)
+        self.msg_id = msg.id
         await msg.add_reaction(self.get_emoji())
-        # emojis = ['✋', '2️⃣', '3️⃣']
-        # for emoji in emojis:
-        #     await msg.add_reaction(emoji)
         
-        # def check(reaction, user):
-        #     are_same_messages = reaction.message.channel == msg.channel and reaction.message.id == msg.id
-        #     return user == ctx.author and are_same_messages and (str(reaction.emoji) == '2️⃣' or str(reaction.emoji) == '3️⃣')
+        def check(reaction, user):
+            are_same_messages = reaction.message.channel == msg.channel and reaction.message.id == msg.id
+            return user == ctx.author and are_same_messages and (str(reaction.emoji) == '2️⃣' or str(reaction.emoji) == '3️⃣')
         
         # timeout = 60 * 60 * 24 * 30 * 2
         # try:
